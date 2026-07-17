293bcd0747676a73fa138648ea5f9a29cd530358
anwar-513 <anwarullah8389@gmail.com>
2026-07-17 12:16:45 +0500

    initial javasript is coded

--- script.js
+++ script.js
@@ -1,1 +1,66 @@
-console.log("Calculator project is started");
+let result = "0";
+let resultArea = document.querySelector(".output-area");
+
+
+function buttonClick(value) {
+    if (isNaN(parseInt(value))) {
+        handleSymble(value)
+    }
+    else {
+        handleNumber(value)
+    }
+
+    rerender();
+}
+
+function handleNumber(number) {
+    if (result === '0') {
+        result = number;
+    }
+    else {
+        result += number;
+    }
+
+}
+
+function handleSymble(symble) {
+    switch (symble) {
+        case 'C':
+            result = 0;
+            break;
+            
+        case '=':
+            break;
+        case '«':
+            if (result.length === 1) {
+                result = '0';
+                break;
+            }
+            else {
+                result = result.substring(0, result.length - 1);
+            }
+
+        case '+':
+        case '-':
+        case '×':
+        case '÷':
+            handleMath(symble);
+            break;
+    }
+
+}
+
+
+function init() {
+    document
+        .querySelector('.buttons')
+        .addEventListener("click", function (event) {
+            buttonClick(event.target.innerText);
+        })
+}
+
+function rerender() {
+    resultArea.innerText = result
+}
+
+init();
